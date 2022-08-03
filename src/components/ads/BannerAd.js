e12004c697ce00433d515ad95b085baecd28a813
Alan <51915832+alanhcrdz@users.noreply.github.com>
2022-08-03 15:50:55 -0300

    Update BannerAd.js

--- src/components/ads/BannerAd.js
+++ src/components/ads/BannerAd.js
@@ -4,11 +4,11 @@ import { Platform } from "react-native";
 import { AdMobBanner } from "expo-ads-admob";
 import * as Device from "expo-device";
 
-const productionAndroid = "ca-app-pub-9871106933538473/9702119337";
-const testAndroid = "ca-app-pub-3940256099942544/6300978111";
+const productionAndroid = "";
+const testAndroid = "";
 
-const productionIos = "ca-app-pub-9871106933538473/1712960432";
-const testIos = "ca-app-pub-3940256099942544/2934735716";
+const productionIos = "";
+const testIos = "";
 
 // UNITS
 const bannerUnit = Platform.select({
